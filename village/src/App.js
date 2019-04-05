649ebcde3775ee2abecd54fc0a9ab4652b1888bf
dgbailey <bailey.dust@gmail.com>
2019-04-05 09:07:22 -0700

    MVP part two complete. Nav routing functioning

--- village/src/App.js
+++ village/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 import axios from 'axios';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   constructor(props) {
@@ -34,15 +35,23 @@ class App extends Component {
       .catch(err => {
         console.log(err);
       });
-    }
+  }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className="App">
-        <SmurfForm addMySmurf={this.addMySmurf}/>
-        <Smurfs smurfs={this.state.smurfs} />
+        <div className="nav-container">
+          <nav>
+            <Link to='/'>Smurfs</Link>
+            <Link to='/smurf-form'>Add Smurf</Link>
+          </nav>
+        </div>
+        <Route path='/' render = {(props) =><Smurfs {...props} smurfs={this.state.smurfs}/>}></Route>
+        <Route exact path='/smurf-form' render = {(props) =><SmurfForm {...props} addMySmurf={this.addMySmurf}/>}></Route>
+        
+        
       </div>
     );
   }
